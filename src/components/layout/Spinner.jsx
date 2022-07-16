2f07846832acc3fb67b317a6e149ce98e61e4f30
ancp <ayenyeinchanpyae98@gmail.com>
2022-07-16 16:39:42 +0630

    add spinner

--- src/components/layout/Spinner.jsx
+++ src/components/layout/Spinner.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import spinner from "./assets/spinner.gif";
 
-const Spinner = () => {
-  return <div>Loading...</div>;
-};
+function Spinner() {
+  return (
+    <div className="w-100 mt-20">
+      <img
+        width={180}
+        className="text-center mx-auto"
+        src={spinner}
+        alt="Loading..."
+      />
+    </div>
+  );
+}
 
 export default Spinner;
